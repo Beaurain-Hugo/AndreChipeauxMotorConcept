b305385499fa9c84e180907cd09abd7b41d52cbc
Beaurain-Hugo <hugo.beaurain@edu.univ-fcomte.fr>
2023-06-02 13:22:00 +0200

    Modification router

--- src/router/index.js
+++ src/router/index.js
@@ -6,8 +6,6 @@ import LivredOrView from '../views/LivredOrView.vue'
 import ContactView from '../views/ContactView.vue'
 import GalerieView from '../views/GalerieView.vue'
 import ExpertiseView from '../views/ExpertiseView.vue'
-import test from '../views/test.vue'
-import test2 from '../views/test2.vue'
 import AjouterAvisView from '../views/AjouterAvisView.vue'
 import AjouterPalmaresView from '../views/AjouterPalmaresView.vue'
 import MentionsLegalesView from '../views/MentionsLegalesView.vue'
@@ -22,8 +20,6 @@ const router = createRouter({
     { path: '/contact', name: 'contact', component: ContactView },
     { path: '/galerie', name: 'galerie', component: GalerieView },
     { path: '/expertise', name: 'expertise', component: ExpertiseView },
-    { path: '/test', name: 'test', component: test },
-    { path: '/test2', name: 'test2', component: test2 },
     { path: '/ajouteravisview', name: 'ajouteravisview', component: AjouterAvisView },
     { path: '/ajouterpalmaresview', name: 'ajouterpalmaresview', component: AjouterPalmaresView },
     { path: '/mentionslegales', name: 'mentionslegalesview', component: MentionsLegalesView },
