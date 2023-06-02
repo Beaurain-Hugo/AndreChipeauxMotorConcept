c83213dafd5efc8d0edd73072f9ccef65c7b279a
Beaurain-Hugo <hugo.beaurain@edu.univ-fcomte.fr>
2023-06-02 15:54:06 +0200

    Suppresion de troisJS

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 import { createApp } from 'vue'
-import { TroisJSVuePlugin } from 'troisjs'
 import App from './App.vue'
 import router from './router'
 import { plugin, defaultConfig } from '@formkit/vue'
@@ -11,6 +10,5 @@ import './style.css'
 const app = createApp(App)
 
 app.use(router)
-app.use(TroisJSVuePlugin)
 app.use(plugin, defaultConfig)
 app.mount('#app')
