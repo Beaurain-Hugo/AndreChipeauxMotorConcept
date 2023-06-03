cb0543abeee70163a594feb18d74efe9b661a986
Beaurain-Hugo <hugo.beaurain@edu.univ-fcomte.fr>
2023-06-03 10:30:27 +0200

    Changement logo dans le menu mobile

--- src/App.vue
+++ src/App.vue
@@ -45,7 +45,7 @@ const menuFermer = ref(true)
         <span class="sr-only">Fermer menu</span> 
       </button>
       <a class="absolute top-3 left-3" href="/index.html">
-        <img src="@/assets/logoMotorConcept.svg" alt="Vue logo" width="200"/>
+        <img src="@/assets/logoMotorConceptCouleur.svg" alt="Vue logo" width="200"/>
       </a>
       <nav class="flex flex-col font-cabin gap-5 text-4xl">
         <RouterLink class="relative z-10" @click="menuOuvert = !menuOuvert, menuFermer = !menuFermer" to="/presentation">Présentation</RouterLink>
@@ -65,7 +65,7 @@ const menuFermer = ref(true)
 
   <footer class="flex flex-col items-center font-cabin md:items-stretch px-7 py-3 bg-Extended/blue-gray/750 text-neutral-50">
     <div class="flex flex-col md:flex-row md:justify-between gap-3 md:gap-0">
-      <a class="my-auto" href="/"><img class="w-full stroke-white" src="@/assets/logoMotorConceptBlanc.svg" alt="Logo d'André Chipeaux Motor Concept"></a>
+      <a class="my-auto" href="/"><img class="w-full stroke-whte" src="@/assets/logoMotorConceptBlanc.svg" alt="Logo d'André Chipeaux Motor Concept"></a>
       <div class="px-8 flex flex-col gap-5">
         <ul class="flex flex-col gap-2 text-sm lg:text-base">
           <li class="flex items-center gap-2"><img class="w-8 text-white" src="@/assets/facebook.png" alt=""> <a href="https://www.facebook.com/andre.chipeaux/" target="_blank">Andre.Chipeaux</a></li>
