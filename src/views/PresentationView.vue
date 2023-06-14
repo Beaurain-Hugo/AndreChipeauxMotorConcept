d105474d4aa8cf5c10e2d4e0b4ec8d49471149a1
Beaurain-Hugo <hugo.beaurain@edu.univ-fcomte.fr>
2023-06-14 17:04:51 +0200

    Changement image carrousel

--- src/views/PresentationView.vue
+++ src/views/PresentationView.vue
@@ -1,25 +1,206 @@
 <script setup>
-import { onMounted } from 'vue';
+import { ref, onMounted } from 'vue';
 import { ChevronDoubleDownIcon } from '@heroicons/vue/24/outline';
+
+const imageWidth = window.innerWidth
+console.log(imageWidth)
+
+const carrouselXS = ref(false)
+const carrouselSM = ref(false)
+const carrouselMD = ref(false)
+const carrouselLG = ref(false)
+const imgSizeCarrousel = () => {
+  if (imageWidth <= 450) {
+    console.log(imageWidth)
+    carrouselXS.value = true
+    carrouselSM.value = false
+    carrouselMD.value = false
+    carrouselLG.value = false
+  } else if (imageWidth <= 640){
+     carrouselXS.value = false
+     carrouselSM.value = true
+     carrouselMD.value = false
+     carrouselLG.value = false
+     console.log(imageWidth)
+  } else if (imageWidth <= 768){
+     carrouselXS.value = false
+     carrouselSM.value = false
+     carrouselMD.value = true
+     carrouselLG.value = false
+      console.log(imageWidth)
+  } else if (768 < imageWidth){
+     carrouselXS.value = false
+     carrouselSM.value = false
+     carrouselMD.value = false
+     carrouselLG.value = true
+     console.log(imageWidth)
+  }
+}
+
 function carrousel() {
     
-    var images15 = document.querySelectorAll('#carrousel');
+    var imagesXS = document.querySelectorAll('#carrouselXS');
     let translate = window.innerWidth;
-    console.log(translate)
+    console.log("translate", translate)
       let d = 2000;
       let delta = 1000;
   
-      images15.forEach(function(img,indice) {
-          img.style.zIndex = images15.length - indice;
+      imagesXS.forEach(function(img,indice) {
+          img.style.zIndex = imagesXS.length - indice;
       });
   
-      let anim15 = anime.timeline({
+      let animXS = anime.timeline({
           loop: true,
           delay: 0
       });
   
-      anim15.add({
-          targets: images15,
+      animXS.add({
+          targets: imagesXS,
+          translateX: [{value: translate, duration: d},
+                       {value: -translate, duration: 0, delay: function(img,ind) {
+                           if (ind == 0) return (10*delta)+(9*d);
+                           if (ind == 1) return (9*delta)+(8*d);
+                           if (ind == 2) return (8*delta)+(7*d);
+                           if (ind == 3) return (7*delta)+(6*d);
+                           if (ind == 4) return (6*delta)+(5*d);
+                           if (ind == 5) return (5*delta)+(4*d);
+                           if (ind == 6) return (4*delta)+(3*d);
+                           if (ind == 7) return (3*delta)+(2*d);
+                           if (ind == 8) return (2*delta)+d;
+                           if (ind == 9) return delta;
+                           return 0;
+                       }},
+                       {value: '0', duration: function(img,ind) {
+                           if (ind == 10) return 0;
+                           return d;
+                       }}],
+          easing: 'linear',
+          delay: function(img,ind) {
+              if (ind == 0) return delta;
+              if (ind == 1) return (2*delta)+d;
+              if (ind == 2) return (3*delta)+(2*d);
+              if (ind == 3) return (4*delta)+(3*d);
+              if (ind == 4) return (5*delta)+(4*d);
+              if (ind == 5) return (6*delta)+(5*d);
+              if (ind == 6) return (7*delta)+(6*d);
+              if (ind == 7) return (8*delta)+(7*d);
+              if (ind == 8) return (9*delta)+(8*d);
+              if (ind == 9) return (10*delta)+(9*d);
+              return (11*delta)+(10*d);
+          }
+      });
+
+      var imagesSM = document.querySelectorAll('#carrouselSM');
+        console.log("translate", translate)
+
+  
+      imagesSM.forEach(function(img,indice) {
+          img.style.zIndex = imagesSM.length - indice;
+      });
+  
+      let animSM = anime.timeline({
+          loop: true,
+          delay: 0
+      });
+  
+      animSM.add({
+          targets: imagesSM,
+          translateX: [{value: translate, duration: d},
+                       {value: -translate, duration: 0, delay: function(img,ind) {
+                           if (ind == 0) return (10*delta)+(9*d);
+                           if (ind == 1) return (9*delta)+(8*d);
+                           if (ind == 2) return (8*delta)+(7*d);
+                           if (ind == 3) return (7*delta)+(6*d);
+                           if (ind == 4) return (6*delta)+(5*d);
+                           if (ind == 5) return (5*delta)+(4*d);
+                           if (ind == 6) return (4*delta)+(3*d);
+                           if (ind == 7) return (3*delta)+(2*d);
+                           if (ind == 8) return (2*delta)+d;
+                           if (ind == 9) return delta;
+                           return 0;
+                       }},
+                       {value: '0', duration: function(img,ind) {
+                           if (ind == 10) return 0;
+                           return d;
+                       }}],
+          easing: 'linear',
+          delay: function(img,ind) {
+              if (ind == 0) return delta;
+              if (ind == 1) return (2*delta)+d;
+              if (ind == 2) return (3*delta)+(2*d);
+              if (ind == 3) return (4*delta)+(3*d);
+              if (ind == 4) return (5*delta)+(4*d);
+              if (ind == 5) return (6*delta)+(5*d);
+              if (ind == 6) return (7*delta)+(6*d);
+              if (ind == 7) return (8*delta)+(7*d);
+              if (ind == 8) return (9*delta)+(8*d);
+              if (ind == 9) return (10*delta)+(9*d);
+              return (11*delta)+(10*d);
+          }
+      });
+
+      var imagesMD = document.querySelectorAll('#carrouselMD');
+    console.log("translate", translate)
+  
+      imagesMD.forEach(function(img,indice) {
+          img.style.zIndex = imagesMD.length - indice;
+      });
+  
+      let animMD = anime.timeline({
+          loop: true,
+          delay: 0
+      });
+  
+      animMD.add({
+          targets: imagesMD,
+          translateX: [{value: translate, duration: d},
+                       {value: -translate, duration: 0, delay: function(img,ind) {
+                           if (ind == 0) return (10*delta)+(9*d);
+                           if (ind == 1) return (9*delta)+(8*d);
+                           if (ind == 2) return (8*delta)+(7*d);
+                           if (ind == 3) return (7*delta)+(6*d);
+                           if (ind == 4) return (6*delta)+(5*d);
+                           if (ind == 5) return (5*delta)+(4*d);
+                           if (ind == 6) return (4*delta)+(3*d);
+                           if (ind == 7) return (3*delta)+(2*d);
+                           if (ind == 8) return (2*delta)+d;
+                           if (ind == 9) return delta;
+                           return 0;
+                       }},
+                       {value: '0', duration: function(img,ind) {
+                           if (ind == 10) return 0;
+                           return d;
+                       }}],
+          easing: 'linear',
+          delay: function(img,ind) {
+              if (ind == 0) return delta;
+              if (ind == 1) return (2*delta)+d;
+              if (ind == 2) return (3*delta)+(2*d);
+              if (ind == 3) return (4*delta)+(3*d);
+              if (ind == 4) return (5*delta)+(4*d);
+              if (ind == 5) return (6*delta)+(5*d);
+              if (ind == 6) return (7*delta)+(6*d);
+              if (ind == 7) return (8*delta)+(7*d);
+              if (ind == 8) return (9*delta)+(8*d);
+              if (ind == 9) return (10*delta)+(9*d);
+              return (11*delta)+(10*d);
+          }
+      });
+
+    var imagesLG = document.querySelectorAll('#carrouselLG');
+    console.log("translate", translate)
+  
+      imagesLG.forEach(function(img,indice) {
+          img.style.zIndex = imagesLG.length - indice;
+      });
+  
+      let animLG = anime.timeline({
+          loop: true,
+          delay: 0
+      });
+  
+      animLG.add({
+          targets: imagesLG,
           translateX: [{value: translate, duration: d},
                        {value: -translate, duration: 0, delay: function(img,ind) {
                            if (ind == 0) return (10*delta)+(9*d);
@@ -57,24 +238,67 @@ function carrousel() {
 
 onMounted(() => {
   carrousel()
+  imgSizeCarrousel()
 })
 </script>
 
 <template>
   <main class="flex flex-col gap-6">
     <div class="p-0 relative overflow-hidden">
-			<div class="h-[90vh] left-0">
-				<img id="carrousel" class="absolute h-[100vh] min-w-full top-0 left-0 z-0" alt="Image de l'atelier d'André Chipeaux" src="../assets/test2.jpg"/>
- 				<img id="carrousel" class="absolute h-[100vh] min-w-full top-0 left-0 z-0" alt="Image de l'atelier d'André Chipeaux" src="../assets/test5.jpg"/>
-        <img id="carrousel" class="absolute h-[100vh] min-w-full top-0 left-0 z-0" alt="Image de l'atelier d'André Chipeaux" src="../assets/test7.jpg"/>
-				<img id="carrousel" class="absolute h-[100vh] min-w-full top-0 left-0 z-0" alt="Image de l'atelier d'André Chipeaux" src="../assets/test6.jpg"/>
-        <img id="carrousel" class="absolute h-[100vh] min-w-full top-0 left-0 z-0" alt="Image de l'atelier d'André Chipeaux" src="../assets/test.jpg"/>
-				<img id="carrousel" class="absolute h-[100vh] min-w-full top-0 left-0 z-0" alt="Image de l'atelier d'André Chipeaux" src="../assets/test11.jpg"/>
-				<img id="carrousel" class="absolute h-[100vh] min-w-full top-0 left-0 z-0" alt="Image de l'atelier d'André Chipeaux" src="../assets/test8.jpg"/>
-				<img id="carrousel" class="absolute h-[100vh] min-w-full top-0 left-0 z-0" alt="Image de l'atelier d'André Chipeaux" src="../assets/test3.jpg"/>
-				<img id="carrousel" class="absolute h-[100vh] min-w-full top-0 left-0 z-0" alt="Image de l'atelier d'André Chipeaux" src="../assets/test4.jpg"/>
-				<img id="carrousel" class="absolute h-[100vh] min-w-full top-0 left-0 z-0" alt="Image de l'atelier d'André Chipeaux" src="../assets/test9.jpg"/>
-        <img id="carrousel" class="absolute h-[100vh] min-w-full top-0 left-0 z-0" alt="Image de l'atelier d'André Chipeaux" src="../assets/test10.jpg"/>
+      <div v-show="carrouselXS" class="h-[90vh] left-0">
+				<img id="carrouselXS" class="absolute h-[100vh] min-w-full top-0 left-0 z-0" alt="Image de l'atelier d'André Chipeaux" src="../assets/imgCarrousel2XS.jpg"/>
+ 				<img id="carrouselXS" class="absolute h-[100vh] min-w-full top-0 left-0 z-0" alt="Image de l'atelier d'André Chipeaux" src="../assets/imgCarrousel5XS.jpg"/>
+        <img id="carrouselXS" class="absolute h-[100vh] min-w-full top-0 left-0 z-0" alt="Image de l'atelier d'André Chipeaux" src="../assets/imgCarrousel7XS.jpg"/>
+				<img id="carrouselXS" class="absolute h-[100vh] min-w-full top-0 left-0 z-0" alt="Image de l'atelier d'André Chipeaux" src="../assets/imgCarrousel6XS.jpg"/>
+        <img id="carrouselXS" class="absolute h-[100vh] min-w-full top-0 left-0 z-0" alt="Image de l'atelier d'André Chipeaux" src="../assets/imgCarrousel1XS.jpg"/>
+				<img id="carrouselXS" class="absolute h-[100vh] min-w-full top-0 left-0 z-0" alt="Image de l'atelier d'André Chipeaux" src="../assets/imgCarrousel11XS.jpg"/>
+				<img id="carrouselXS" class="absolute h-[100vh] min-w-full top-0 left-0 z-0" alt="Image de l'atelier d'André Chipeaux" src="../assets/imgCarrousel8XS.jpg"/>
+				<img id="carrouselXS" class="absolute h-[100vh] min-w-full top-0 left-0 z-0" alt="Image de l'atelier d'André Chipeaux" src="../assets/imgCarrousel3XS.jpg"/>
+				<img id="carrouselXS" class="absolute h-[100vh] min-w-full top-0 left-0 z-0" alt="Image de l'atelier d'André Chipeaux" src="../assets/imgCarrousel4XS.jpg"/>
+				<img id="carrouselXS" class="absolute h-[100vh] min-w-full top-0 left-0 z-0" alt="Image de l'atelier d'André Chipeaux" src="../assets/imgCarrousel9XS.jpg"/>
+        <img id="carrouselXS" class="absolute h-[100vh] min-w-full top-0 left-0 z-0" alt="Image de l'atelier d'André Chipeaux" src="../assets/imgCarrousel10XS.jpg"/>
+
+			</div>
+      <div v-show="carrouselSM" class="h-[90vh] left-0">
+				<img id="carrouselSM" class="absolute h-[100vh] min-w-full top-0 left-0 z-0" alt="Image de l'atelier d'André Chipeaux" src="../assets/imgCarrousel2SM.jpg"/>
+ 				<img id="carrouselSM" class="absolute h-[100vh] min-w-full top-0 left-0 z-0" alt="Image de l'atelier d'André Chipeaux" src="../assets/imgCarrousel5SM.jpg"/>
+        <img id="carrouselSM" class="absolute h-[100vh] min-w-full top-0 left-0 z-0" alt="Image de l'atelier d'André Chipeaux" src="../assets/imgCarrousel7SM.jpg"/>
+				<img id="carrouselSM" class="absolute h-[100vh] min-w-full top-0 left-0 z-0" alt="Image de l'atelier d'André Chipeaux" src="../assets/imgCarrousel6SM.jpg"/>
+        <img id="carrouselSM" class="absolute h-[100vh] min-w-full top-0 left-0 z-0" alt="Image de l'atelier d'André Chipeaux" src="../assets/imgCarrousel1SM.jpg"/>
+				<img id="carrouselSM" class="absolute h-[100vh] min-w-full top-0 left-0 z-0" alt="Image de l'atelier d'André Chipeaux" src="../assets/imgCarrousel11SM.jpg"/>
+				<img id="carrouselSM" class="absolute h-[100vh] min-w-full top-0 left-0 z-0" alt="Image de l'atelier d'André Chipeaux" src="../assets/imgCarrousel8SM.jpg"/>
+				<img id="carrouselSM" class="absolute h-[100vh] min-w-full top-0 left-0 z-0" alt="Image de l'atelier d'André Chipeaux" src="../assets/imgCarrousel3SM.jpg"/>
+				<img id="carrouselSM" class="absolute h-[100vh] min-w-full top-0 left-0 z-0" alt="Image de l'atelier d'André Chipeaux" src="../assets/imgCarrousel4SM.jpg"/>
+				<img id="carrouselSM" class="absolute h-[100vh] min-w-full top-0 left-0 z-0" alt="Image de l'atelier d'André Chipeaux" src="../assets/imgCarrousel9SM.jpg"/>
+        <img id="carrouselSM" class="absolute h-[100vh] min-w-full top-0 left-0 z-0" alt="Image de l'atelier d'André Chipeaux" src="../assets/imgCarrousel10SM.jpg"/>
+SM
+			</div>   
+			<div v-show="carrouselMD" class="h-[90vh] left-0">
+				<img id="carrouselMD" class="absolute h-[100vh] min-w-full top-0 left-0 z-0" alt="Image de l'atelier d'André Chipeaux" src="../assets/imgCarrousel2SM.jpg"/>
+ 				<img id="carrouselMD" class="absolute h-[100vh] min-w-full top-0 left-0 z-0" alt="Image de l'atelier d'André Chipeaux" src="../assets/imgCarrousel5SM.jpg"/>
+        <img id="carrouselMD" class="absolute h-[100vh] min-w-full top-0 left-0 z-0" alt="Image de l'atelier d'André Chipeaux" src="../assets/imgCarrousel7SM.jpg"/>
+				<img id="carrouselMD" class="absolute h-[100vh] min-w-full top-0 left-0 z-0" alt="Image de l'atelier d'André Chipeaux" src="../assets/imgCarrousel6SM.jpg"/>
+        <img id="carrouselMD" class="absolute h-[100vh] min-w-full top-0 left-0 z-0" alt="Image de l'atelier d'André Chipeaux" src="../assets/imgCarrousel1SM.jpg"/>
+				<img id="carrouselMD" class="absolute h-[100vh] min-w-full top-0 left-0 z-0" alt="Image de l'atelier d'André Chipeaux" src="../assets/imgCarrousel11SM.jpg"/>
+				<img id="carrouselMD" class="absolute h-[100vh] min-w-full top-0 left-0 z-0" alt="Image de l'atelier d'André Chipeaux" src="../assets/imgCarrousel8SM.jpg"/>
+				<img id="carrouselMD" class="absolute h-[100vh] min-w-full top-0 left-0 z-0" alt="Image de l'atelier d'André Chipeaux" src="../assets/imgCarrousel3SM.jpg"/>
+				<img id="carrouselMD" class="absolute h-[100vh] min-w-full top-0 left-0 z-0" alt="Image de l'atelier d'André Chipeaux" src="../assets/imgCarrousel4SM.jpg"/>
+				<img id="carrouselMD" class="absolute h-[100vh] min-w-full top-0 left-0 z-0" alt="Image de l'atelier d'André Chipeaux" src="../assets/imgCarrousel9SM.jpg"/>
+        <img id="carrouselMD" class="absolute h-[100vh] min-w-full top-0 left-0 z-0" alt="Image de l'atelier d'André Chipeaux" src="../assets/imgCarrousel10SM.jpg"/>
+
+			</div>
+       <div v-show="carrouselLG" class="h-[90vh] left-0">
+				<img id="carrouselLG" class="absolute h-[100vh] min-w-full top-0 left-0 z-0" alt="Image de l'atelier d'André Chipeaux" src="../assets/test2.jpg"/>
+ 				<img id="carrouselLG" class="absolute h-[100vh] min-w-full top-0 left-0 z-0" alt="Image de l'atelier d'André Chipeaux" src="../assets/test5.jpg"/>
+        <img id="carrouselLG" class="absolute h-[100vh] min-w-full top-0 left-0 z-0" alt="Image de l'atelier d'André Chipeaux" src="../assets/test7.jpg"/>
+				<img id="carrouselLG" class="absolute h-[100vh] min-w-full top-0 left-0 z-0" alt="Image de l'atelier d'André Chipeaux" src="../assets/test6.jpg"/>
+        <img id="carrouselLG" class="absolute h-[100vh] min-w-full top-0 left-0 z-0" alt="Image de l'atelier d'André Chipeaux" src="../assets/test.jpg"/>
+				<img id="carrouselLG" class="absolute h-[100vh] min-w-full top-0 left-0 z-0" alt="Image de l'atelier d'André Chipeaux" src="../assets/test11.jpg"/>
+				<img id="carrouselLG" class="absolute h-[100vh] min-w-full top-0 left-0 z-0" alt="Image de l'atelier d'André Chipeaux" src="../assets/test8.jpg"/>
+				<img id="carrouselLG" class="absolute h-[100vh] min-w-full top-0 left-0 z-0" alt="Image de l'atelier d'André Chipeaux" src="../assets/test3.jpg"/>
+				<img id="carrouselLG" class="absolute h-[100vh] min-w-full top-0 left-0 z-0" alt="Image de l'atelier d'André Chipeaux" src="../assets/test4.jpg"/>
+				<img id="carrouselLG" class="absolute h-[100vh] min-w-full top-0 left-0 z-0" alt="Image de l'atelier d'André Chipeaux" src="../assets/test9.jpg"/>
+        <img id="carrouselLG" class="absolute h-[100vh] min-w-full top-0 left-0 z-0" alt="Image de l'atelier d'André Chipeaux" src="../assets/test10.jpg"/>
 
 			</div>
       <h1 class="absolute z-10 text-4xl font-bona-nova font-bold text-center text-white left-1/2 -translate-x-1/2 top-24 leading-none">André Chipeaux</h1>
