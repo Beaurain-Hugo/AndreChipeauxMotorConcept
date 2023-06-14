83d422f4386662e0709eacc28f08b5eede9e29e2
Beaurain-Hugo <hugo.beaurain@edu.univ-fcomte.fr>
2023-06-14 17:52:08 +0200

    Changement affichage information palmarès

--- src/components/cardVoiture.vue
+++ src/components/cardVoiture.vue
@@ -9,8 +9,8 @@
 </script>
 
 <template>
-    <div class="group max-w-min">
-        <div class="px-3 hidden m-auto py-1 font-cabin bg-gray-300 border-2 border-black w-max group-hover:block">
+    <div class="group relative max-w-min">
+        <div class="px-3 hidden m-auto py-1 font-cabin bg-gray-300 top-[-9rem] border-2 absolute border-black w-max group-hover:block">
             <p class="text-2xl after:inline-block after:align-middle after:mx-1 after:h-0.5 after:w-10 after:content-[''] after:bg-black">{{ nomPilote }}</p>
             <div class="flx gap- after:inline-block after:align-middle after:mx-1 after:h-0.5 after:w-10 after:content-[''] after:bg-black">
                 <p class="after:inline-block after:align-middle after:mx-1 after:h-0.5 after:w- after:content-[''] after:bg-black">{{ place }}</p>
--- src/views/PalmaresView.vue
+++ src/views/PalmaresView.vue
@@ -19,7 +19,7 @@ const { data: voitures, error } = await supabase
         <h1 class="text-4xl font-bona-nova font-bold text-center text-Extended/blue-gray/750 leading-none">Palmarès</h1>
       </div>
       <img class="h-[200vh]" src="@/assets/palmaresIllustration.jpg" alt="">
-      <div class="absolute">
+      <div class="absolute mt-16">
       <div class="first:mt-36 odd:ml-[10vw] odd:sm:ml-[20vw] md:odd:ml-[25vw] lg:odd:ml-[30vw] 2xl:odd:ml-[35vw] even:ml-[60vw]" v-for="voiture in voitures" :key="voiture.id">
             <cardVoiture v-bind="voiture" />
         </div>
